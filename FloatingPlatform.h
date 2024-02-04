47ff319db243ee92855b1eb6cc2cea9b6e37c0d0
Abhay Singh <106145602+inventwithdean@users.noreply.github.com>
2024-02-04 23:58:13 +0530

    Updated versions

--- FloatingPlatform.h
+++ FloatingPlatform.h
@@ -14,41 +14,25 @@ class FIRSTPROJECT_API AFloatingPlatform : public AActor
 public:	
 	// Sets default values for this actor's properties
 	AFloatingPlatform();
-
-	/** Mesh of the Platform */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Platform")
-	UStaticMeshComponent* PlatformMesh;
-
-	/** Start position of Platform */
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Platform")
-	FVector StartPos;
-
-	/** End position of Platform */
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Platform", meta = (MakeEditWidget = "true"))
-	FVector EndPos;
-
-	/** Bool to keep track of whether Platform is interpolating or not */
-	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = "Platform")
-	bool bIsInterping;
-
-	/** Floating Platform will take *InterpDuration seconds to reach next position */
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Platform")
-	float InterpDuration;
-
-	/** Time in Seconds to pause at Start and End positions */
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Platform")
-	float LerpPause;
-
-	/** Float to keep track of Time elapsed in seconds
-	/** It is being reset at end of each positions to make platform back and forth */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Platform")
-	float TimeElapsed;
-
-	// *** IMPORTANT ***
-	// It can't have any Macro Specifiers!
+	UPROPERTY(VisibleAnywhere, Category=Platform)
+	class UStaticMeshComponent* PlatformMesh;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Platform)
+	FVector StartPoint;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Platform, meta=(MakeEditWidget="true"))
+	FVector EndPoint;
+	UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category=Platform)
+	float InterpTime;
+	UPROPERTY(VisibleInstanceOnly)
+	float TimeElapsedSinceLastStop;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Platform)
+	bool bShouldInterp;
+	FTimerHandle InterpTimer;
+	UPROPERTY(EditDefaultsOnly, Category=Platform)
+	float WaitingTime;
+	UPROPERTY(VisibleAnywhere)
+	float DistanceToTravel;
 	UPROPERTY()
-	FTimerHandle PlatformTimer;
-
+	FVector UnitVectorInDirectionOfTravel;
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -56,10 +40,7 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
-	/** Function which will be called by Timer to Toggle bIsInterping bool so that it can be used to move platform */
-	void ToggleInterping();
-
-	/** Function to swap to vectors, which will help in swapping start and end positions */
-	void SwapVectors(FVector& VecOne, FVector& VecTwo);
+	void SwapVectors(FVector& Vec1, FVector& Vec2);
+	UFUNCTION()
+	void ToggleInterp();
 };
